8d4388e6c9155209aa2baf89e0716a2d61d85a08
Eric Colgan <S00172004@mail.itsligo.ie>
2020-07-05 16:47:17 +0100

    pls work

---
+++ Week 1/Week 1/Migrations/202007051517357_week1.cs
@@ -0,0 +1,29 @@
+﻿namespace Week_1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class week1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        MidName = c.String(),
+                        LastName = c.String(),
+                        DOB = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.User");
+        }
+    }
+}
--- Week 1/Week 1/Migrations/Configuration.cs
+++ Week 1/Week 1/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 ﻿namespace Week_1.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Week_1.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Week_1.Data.CoreContext>
     {
@@ -18,6 +20,22 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            var users = new List<User>
+            {
+                new User(){FirstName = "Billy", MidName = "Joe", LastName = "McBilly",
+                    DOB = DateTime.Parse("1980-01-01")},
+
+            new User()
+            {
+                FirstName = "John",
+                MidName = "Jeff",
+                LastName = "Jonathan",
+                DOB = DateTime.Parse("1980-01-01")
+            }
+        };
+            users.ForEach(u => context.Users.Add(u));
+            base.Seed(context);
         }
     }
 }
